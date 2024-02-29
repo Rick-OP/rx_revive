5d34ee23d0d11243c1064992eec329e85bf6d644
RICK-OP <69990956+Rick-OP@users.noreply.github.com>
2024-03-01 02:40:53 +0530

    rework

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,14 @@
---------------------------------
---- RP Revive, Made by FAXES ---
---------------------------------
+----------------------------------
+--- RP Revive, Made by RICK-OP ---
+----------------------------------
 
 fx_version 'bodacious'
 game 'gta5'
 
 author 'RICK-OP/RICKX'
-description 'read the readme.md'
+repository "https://github.com/Rick-OP/RX-RPRevive"
+description 'A simple revive Script. read the readme.md'
+version "v1.0.2"
 
 client_script 'client.lua'
 server_script 'server.lua'
